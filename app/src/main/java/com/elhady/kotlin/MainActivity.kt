73a5b5e4759be1401f78569adb091892a917a3f6
islamelhady <i.elhady44@gmail.com>
2021-03-20 04:06:37 +0200

    add navigation

---
+++ app/src/main/java/com/elhady/kotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.elhady.kotlin
+
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        hideActionBar()
+
+    }
+
+    override fun onSupportNavigateUp() =
+        Navigation.findNavController(this, R.id.post_list_fragment).navigateUp()
+
+    private fun hideActionBar() {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.supportActionBar?.hide()
+    }
+
+}
---
+++ app/src/main/java/com/elhady/kotlin/ui/main/view/splash/SplashFragment.kt
@@ -0,0 +1,34 @@
+package com.elhady.kotlin.ui.main.view.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.elhady.kotlin.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        GlobalScope.launch {
+            delay(3000L)
+            findNavController().navigate(
+                R.id.postListFragment
+            )
+        }
+    }
+}
